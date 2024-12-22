2a192b444a6fd9c7e42086bb4665cc7afafb986e
Krystian Pasierb-Suchy <krystian.ps@outlook.com>
2024-12-22 18:53:31 +0100

    Create core entires for db creation

---
+++ Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBoards.Entities
+{
+    public class Comment
+    {
+        public string Message { get; set; }
+        public string Author { get; set; }
+
+        public DateTime CreatedCommentDate { get; set; }
+        public DateTime? UpdatedCommentDate { get; set; }
+
+    }
+}
