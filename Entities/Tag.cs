84d5caf00212cb65d68a3be8a721afea94033278
Krystian Pasierb-Suchy <krystian.ps@outlook.com>
2024-12-24 07:44:52 +0100

    resolve issue with tag merge problem

--- Entities/Tag.cs
+++ Entities/Tag.cs
@@ -4,10 +4,6 @@
     {
         public int Id { get; set; }
         public string Value { get; set; }
-<<<<<<< HEAD
-=======
-        public string Category { get; set; }
->>>>>>> 6b9643e3b9d2034939d40569d23d02fed8450f75
         public List<WorkItem> WorkItems { get; set; } = new List<WorkItem>();
 
     }
